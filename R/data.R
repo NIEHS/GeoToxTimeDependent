0adcb968b59e8331e2d80cd67e6d032075c20880
Kruse <krusepm@nih.gov>
2025-06-26 10:06:16 -0400

    Added ctxR data domain names.

--- R/data.R
+++ R/data.R
@@ -18,3 +18,27 @@
 #'   \item{boundaries}{County and state boundaries for North Carolina in 2019.}
 #' }
 "geo_tox_data"
+
+
+#' Chemical API Server url
+#'
+#' A section of url used in Chemical API Endpoints
+'chemical_api_server'
+
+#' Hazard API Server url
+#'
+#' A section of url used in Hazard API Endpoints
+'hazard_api_server'
+
+#' Bioactivity API Server url
+#'
+#' A section of url used in Bioactivity API Endpoints
+'bioactivity_api_server'
+
+#' Exposure API Server url
+#'
+#' A section of url used in Exposure API Endpoints
+'exposure_api_server'
+
+utils::globalVariables(c('chemical_api_server', 'hazard_api_server',
+                         'bioactivity_api_server', 'exposure_api_server'))
