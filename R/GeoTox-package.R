da5b4e7fcdecb90a1af3ad4e608fb43ba1d9ed11
Kruse <krusepm@nih.gov>
2025-06-26 10:06:43 -0400

    Included new imports.

--- R/GeoTox-package.R
+++ R/GeoTox-package.R
@@ -2,8 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom methods is
 #' @importFrom rlang .data
 #' @importFrom rlang .env
 #' @importFrom sf st_as_sf
+#' @importFrom stats median
 ## usethis namespace: end
 NULL
